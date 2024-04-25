3fd53c040c463ccba9aead2fdbcd2de943f71e91
Yenitu <89053526+Yenitu@users.noreply.github.com>
2024-04-24 23:24:47 -0400

    Create Main.py
    Imports + Screen creation

---
+++ Main.py
@@ -0,0 +1,15 @@
+import asyncio
+import pygame as pig
+import js
+import io
+import random
+import struct
+import sys
+import os
+
+#Create the screen
+js.document.body.style.background = 'Black'
+canvas = js.window.canvas
+canvas.style.outline = 'none'
+canvas.width = 1920
+canvas.height = 1080
